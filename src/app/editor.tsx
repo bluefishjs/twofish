ad2b5128dafd1f9d0625b07c1311e45abd0833b6
Josh Pollock <jopo@mit.edu>
2023-07-25 15:47:43 -0400

    add reactflow

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -9,6 +9,15 @@ import {
 import "@tldraw/tldraw/tldraw.css";
 import { useCallback, useEffect, useState } from "react";
 
+import ReactFlow from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export default function Editor() {
   const [uiEvents, setUiEvents] = useState<string[]>([]);
 
@@ -102,6 +111,9 @@ export default function Editor() {
             {storeEvents.map((t, i) => (
               <div key={i}>{t}</div>
             ))}
+          </div>
+          <div style={{ width: "100vw", height: "100vh" }}>
+            <ReactFlow nodes={initialNodes} edges={initialEdges} />
           </div>
         </div>
       </div>
